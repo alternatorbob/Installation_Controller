610c409a824bbe97f1b7341892a7ef29fccf30b5
Glitch (glitch-hello-website) <none>
2022-09-04 03:59:20 +0000

    🍉🐉 Checkpoint ./script.js:8331381/585

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-function setupWebSocket(socketURL) {
-  let ws = new WebSocket(socketURL);
 
-  ws.onopen = event => {
-    console.log('Socket connection open');
-    // alert('Successfully connected to socket server 🎉');
+let ws = new WebSocket('wss://new-socket-server.herokuapp.com:443');
+
+
+ws.onopen = event => {
+  console.log('Socket connection open');
+  // alert('Successfully connected to socket server 🎉');
+  ws.send('pong');
+}
+
+ws.onmessage = (message) => {
+  if(message && message.data && message.data === 'ping') {
+    console.log('got ping')
     ws.send('pong');
   }
-
-  ws.onmessage = (message) => {
-    if(message === 'ping') {
-      ws.send('pong');
-    }
-    console.log('message', message)
-  }
-
-  ws.onerror = (error) => {
-    console.log('Error in the connection', error);
-    alert('error connecting socket server', error);
-  }
-
-  ws.onclose = (event) => {
-    console.log('Socket connection closed');
-    alert('closing socket server');
-  }
-  return ws;
+  // let message1 = JSON.parse(message);
   
+  console.log('message', message1)
 }
+
+ws.onerror = (error) => {
+  console.log('Error in the connection', error);
+  alert('error connecting socket server', error);
+}
+
+ws.onclose = (event) => {
+  console.log('Socket connection closed');
+  alert('closing socket server');
+}
+
+
