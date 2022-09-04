84e3f1de92152f625c6ff0ccc44ed26c8da2155f
Glitch (glitch-hello-website) <none>
2022-09-04 04:11:13 +0000

    🚴👹 Checkpoint ./script.js:8331381/1033 ./index.html:8331381/23

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let ws = new WebSocket('wss://new-socket-server.herokuapp.com:443');
 
 
+
 ws.onopen = event => {
   console.log('Socket connection open');
   // alert('Successfully connected to socket server 🎉');
@@ -9,13 +10,19 @@ ws.onopen = event => {
 }
 
 ws.onmessage = (message) => {
-  if(message && message.data && message.data === 'ping') {
-    console.log('got ping')
-    ws.send('pong');
+  if(message && message.data) {
+    if(message.data === 'ping') {
+      console.log('got ping')
+      ws.send('pong');  
+      return
+    }
+    let data = JSON.parse(message.data);
+    if(data) {
+      console.log('got data', data);
+    }
   }
-  // let message1 = JSON.parse(message);
   
-  console.log('message', message1)
+  console.log('message', message)
 }
 
 ws.onerror = (error) => {
@@ -29,3 +36,9 @@ ws.onclose = (event) => {
 }
 
 
+let button = document.querySelector('.myButton')
+button.addEventListener('click', () => {
+  console.log('sent')
+  let x = {'button1': 1};
+  ws.send(JSON.stringify(x))
+})
