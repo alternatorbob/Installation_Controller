1b15b252c6ff70108c2b84a81e312ae1d85b94e3
Glitch (glitch-hello-website) <none>
2022-10-29 12:46:44 +0000

    💑👩‍👩‍👦 Checkpoint ./script.js:6685726/2:50408848/2459 ./index.html:6685726/1641

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-let ws = new WebSocket('wss://our-socket-server.herokuapp.com/:443');
+let ws = new WebSocket("wss://touchdesigner-controller.herokuapp.com/:443");
 
-let controllTD = document.querySelector('.controllTD') ;
-controllTD.addEventListener('input', (event) => {
-  ws.send(JSON.stringify({ 'slider1': controllTD.value / 100 }));
-}, false);
+let controllTD = document.querySelector(".controllTD");
+controllTD.addEventListener(
+  "input",
+  (event) => {
+    ws.send(JSON.stringify({ slider1: controllTD.value / 100 }));
+  },
+  false
+);
 
-let controlledByTD = document.querySelector('.controlledByTD');
+let controlledByTD = document.querySelector(".controlledByTD");
 
-ws.addEventListener('open', (event) => {
-  console.log('Socket connection open');
+ws.addEventListener("open", (event) => {
+  console.log("Socket connection open");
   // alert('Successfully connected to socket server 🎉');
-  ws.send('pong');
+  ws.send("pong");
 });
 
-ws.addEventListener('message', (message) => {
+ws.addEventListener("message", (message) => {
   if (message && message.data) {
     if (message.data === "ping") {
       console.log("got ping");
@@ -28,15 +32,15 @@ ws.addEventListener('message', (message) => {
       console.log("got data", data);
     }
   }
-  console.log("message", message)
+  console.log("message", message);
 });
 
-ws.addEventListener('error', (error) => {
-    console.error('Error in the connection', error);
-    alert('error connecting socket server', error);
+ws.addEventListener("error", (error) => {
+  console.error("Error in the connection", error);
+  alert("error connecting socket server", error);
 });
 
-ws.addEventListener('close', (event) => {
-    console.log('Socket connection closed');
-    alert('closing socket server');
+ws.addEventListener("close", (event) => {
+  console.log("Socket connection closed");
+  alert("closing socket server");
 });
